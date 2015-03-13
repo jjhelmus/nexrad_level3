4a50adb496571704ba5b69162539b4ed7f1d29cc
Jonathan Helmus <jjhelmus@gmail.com>
2015-03-13 12:45:41 -0500

    Add qplot.py for quickly plotting files

---
+++ qplot.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+""" Python NEXRAD Level 3 files. """
+import matplotlib.pyplot as plt
+import nexradl3file
+
+SAMPLE_FILE = 'sample_data/KBMX_SDUS54_N0QBMX_201501020205'
+nfile = nexradl3file.NexradLevel3File(SAMPLE_FILE)
+plt.imshow(nfile.data)
+plt.show()
