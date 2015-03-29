5d50d0cc9e6ee982884a1eabd6e360f4e778a24a
Jonathan Helmus <jjhelmus@gmail.com>
2015-03-29 18:29:47 -0500

    Updated project

--- qplot.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-""" Python NEXRAD Level 3 files. """
-import matplotlib.pyplot as plt
-import nexradl3file
-
-SAMPLE_FILE = 'sample_data/KBMX_SDUS54_N0QBMX_201501020205'
-nfile = nexradl3file.NexradLevel3File(SAMPLE_FILE)
-plt.imshow(nfile.get_data())
-plt.show()
